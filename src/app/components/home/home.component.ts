5bb4ad8046ba33b2651cc140d856f0c8759b308e
Ramiro Cuenca <ramicuenca00@gmail.com>
2020-12-14 13:34:36 -0300

    Used HTTP methods for the first time

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  countries: any[] = [];
+
+  constructor(
+    private http: HttpClient,
+  ) {
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+    .subscribe( (response:any) => {
+      this.countries = response;
+      console.log(response);
+    })
+   }
 
   ngOnInit(): void {
   }
